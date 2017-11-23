b2189697f8ba11ea34963a20d10dba308a0ef8b6
Alan Szepieniec <alan.szepieniec@esat.kuleuven.be>
2017-11-23 17:38:19 +0100

    towards working C implementation with updated parameters

---
+++ Reference_Implementation/Ramstake_BCH_216091/api.h
@@ -0,0 +1,11 @@
+#define CRYPTO_SECRETKEYBYTES 5542
+#define CRYPTO_PUBLICKEYBYTES 2787
+#define CRYPTO_BYTES 32
+#define CRYPTO_CIPHERTEXTBYTES 4030
+
+#define CRYPTO_ALGNAME "Ramstake RS 22040"
+
+int crypto_kem_keypair( unsigned char *pk, unsigned char *sk );
+int crypto_kem_enc( unsigned char *ct, unsigned char *ss, const unsigned char *pk );
+int crypto_kem_dec( unsigned char *ss, const unsigned char *ct, const unsigned char *sk );
+
